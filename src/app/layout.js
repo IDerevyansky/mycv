e643d20117d673f67c336afbe03d3a8d699489dc
IDerevyansky <igor.derevyansky@gmail.com>
2024-05-29 22:07:19 -0700

    add meta description

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import './components/globals.css'
 
 export const metadata = {
   title: 'IDerevianskii',
-  description: 'About me',
+  description: 'Have a good day. My name is Igor Derevianskii, I specialize in professionally creating user interface design and development. Throughout my professional career, I have designed numerous products, with some of them becoming flagship products for companies. My experience primarily focuses on visually processing big volumes of data, as I have dedicated a significant portion of my professional activity to the financial sector. My work involves turning complex into straightforward ones. I have been in the industry for over 13 years and hold a Bachelor of Computer Science degree, which allows me to delve deeper into the technical aspects of products. Additionally, I am proficient in product development as a software engineer. If you are interested in my experience, then contact me.',
 }
 
 
