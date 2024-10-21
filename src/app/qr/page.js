39e1a25de79e3d71280d325b72d3dfabcaa7767b
IDerevyansky <igor.derevyansky@gmail.com>
2024-10-21 15:08:19 -0700

    add page qr

---
+++ src/app/qr/page.js
@@ -0,0 +1,39 @@
+import Menu from '../components/menu/menu';
+import Image from 'next/image';
+import './qr.css';
+
+import qrCode from '@/app/lib/img/QRCode.jpg'
+
+
+
+export default function Qr(){
+
+
+    return(
+
+        <main>
+
+            <Menu/>
+    
+            <div className="bodyMainContainer">
+    
+            <div className="bodyContainerPortfolio">
+    
+            <Image
+            src={qrCode}
+            width="320"
+            height="320"
+            alt="Qr code"
+            />
+
+    
+            </div>
+    
+            </div>
+  
+        
+  
+      </main>
+
+    )
+}
